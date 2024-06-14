55301ebe52271e6f57d0a10968df0fa5aec942f3
DESKTOP-HBIBUUM\user <test@test.com>
2024-06-14 15:58:30 +0900

    0613까지 수업(example)

--- src/main/java/com/example/lesson02/Lesson02Ex01RestController.java
+++ src/main/java/com/example/lesson02/Lesson02Ex01RestController.java
@@ -2,15 +2,23 @@ package com.example.lesson02;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson02.bo.UsedGoodsBO;
+import com.example.lesson02.domain.UsedGoods;
 
 @RestController // @Controller + @ResponseBody (JSON)
 public class Lesson02Ex01RestController {
 
+	@Autowired
+	private UsedGoodsBO usedGoodsBO; // DI : Spring bean 주입
+	
 	// http://localhost/lesson02/ex01
 	@RequestMapping("/lesson02/ex01")
 	public List<UsedGoods> ex01() {
-		
+		List<UsedGoods> usedGoodsList = usedGoodsBO.getUsedGoodsList();
+		return usedGoodsList; // list 로 리턴이 되지만 -> Response로 내려가면 JSON String
 	}
 }
