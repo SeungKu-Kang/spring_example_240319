133335a1b2b5c9539a9773dc4574242b1bfaaa20
DESKTOP-HBIBUUM\user <test@test.com>
2024-06-17 18:50:26 +0900

    0617 select,update 학원수업 ex

--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,6 +1,7 @@
 package com.example.lesson03.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.lesson03.domain.Review;
 
@@ -10,4 +11,18 @@ public interface ReviewMapper {
 	// input : id
 	// output : Review or null
 	public Review selectReviewById(int id);
+	
+	public int insertReview(Review review);
+	
+	// @Param 하나의 맵으로 xml에 전송
+	public int insertReviewAsField(
+			@Param("storeId") int storeId12312313, 
+			@Param("menu") String menu122, 
+			@Param("userName") String userName3124123, 
+			@Param("point") Double point123, 
+			@Param("review") String review);
+	
+	public int updateReviewById(
+			@Param("id") int id, 
+			@Param("review") String review);
 }
