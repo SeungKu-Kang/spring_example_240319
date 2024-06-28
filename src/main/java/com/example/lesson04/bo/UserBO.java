f88e0ff7a7df96b682b033c4e5d5a0101834bf9d
DESKTOP-HBIBUUM\user <test@test.com>
2024-06-28 18:51:22 +0900

    0628 학원 수업

--- src/main/java/com/example/lesson04/bo/UserBO.java
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -8,20 +8,26 @@ import com.example.lesson04.mapper.UserMapper;
 
 @Service
 public class UserBO {
-	
+
 	@Autowired
 	private UserMapper userMapper;
+
 	// input : 파라미터 4개
 	// output : X
-	public void addUser(String name, String yyyymmdd,
-			String email, String introduce) {
-		
+	public void addUser(String name, String yyyymmdd, String email, String introduce) {
+
 		userMapper.insertUser(name, yyyymmdd, email, introduce);
 	}
-	
+
 	// input : X
 	// output : User
 	public User getLatestUser() {
 		return userMapper.selectLatestUser();
+	}
+
+	// input: name
+	// output: boolean
+	public boolean isDuplicationByName(String name) {
+		return userMapper.isDuplicationByName(name);
 	}
 }
