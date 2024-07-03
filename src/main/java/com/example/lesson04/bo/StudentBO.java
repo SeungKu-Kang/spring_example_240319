9c29654072d5538e4e5f2f8d372c72f3e876c4f8
DESKTOP-HBIBUUM\user <test@test.com>
2024-07-03 18:51:11 +0900

    0703 학원수업 ex

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -1,6 +1,7 @@
 package com.example.lesson04.bo;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -46,4 +47,42 @@ public class StudentBO {
 	public Student getStudentById(int id) {
 		return studentMapper.selectStudentById(id);
 	}
+	
+	// JPA - update
+	
+	// input : id, dreamJob 
+	// output: StudentEntity - jpa는 엔티티를 리턴한다. or null (업데이트 대상이 없는 경우 null)
+	public StudentEntity updateStudentDreamJobById (int id, String dreamJob) {		
+		// 기존 데이터 조회 => StudentEntity
+		StudentEntity student = studentRepository.findById(id).orElse(null);
+		
+		// Entity의 데이터 값을 변경해놓는다.(update)
+		if (student != null) {
+			student = student.toBuilder() // toBuilder는 기존 필드값을 유지하고 일부만 변경
+					.dreamJob(dreamJob)
+					.build(); // 꼭 객체에 다시 저장해야함 (바뀐것 적용)
+			// save 요청
+			student = studentRepository.save(student);
+		}
+		
+		// update
+		
+		return student;
+	}
+	
+	// input : id
+	// output: X
+	public void deleteStudentById(int id) {
+		// 방법 1)
+//		StudentEntity student = studentRepository.findById(id).orElse(null);
+//		if (student != null) {
+//			studentRepository.delete(student);
+//		}
+		
+		// 방법 2) Optional 활용하기
+		Optional<StudentEntity> studentOptional = studentRepository.findById(id);
+		studentOptional.ifPresent(s -> studentRepository.delete(s));
+		
+	}
+	
 }
--- src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
+++ src/main/java/com/example/lesson07/Lesson07Ex01RestController.java
@@ -14,6 +14,7 @@ public class Lesson07Ex01RestController {
 
 	@Autowired
 	private StudentBO studentBO;
+	
 	// C: create
 	@GetMapping("/create")
 	public StudentEntity create() {
@@ -25,4 +26,34 @@ public class Lesson07Ex01RestController {
 		return studentBO.addStudent(name, phoneNumber, email, dreamJob);
 		
 	}
+	
+	// U : update
+	@GetMapping("/update")
+	public StudentEntity update() {
+		// id가 7번인 것의 dreamJob을 '간호사'에서 '기획자'로 변경
+		return studentBO.updateStudentDreamJobById(7, "기획자");
+	}
+	
+	// D : delete
+	@GetMapping("/delete")
+	public String delete() {
+		// id가 10번인 데이터 삭제
+		studentBO.deleteStudentById(5);
+		
+		return "삭제 성공";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
